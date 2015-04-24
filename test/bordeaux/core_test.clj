6514a5bccb26ffa88e3ee5a1d02944342fbcbec3
Edward Cho <zerokarmaleft@gmail.com>
2015-04-24 15:11:42 -0500

    remove lein scaffold test

--- test/bordeaux/core_test.clj
+++ test/bordeaux/core_test.clj
@@ -1,7 +1,4 @@
 (ns bordeaux.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test  :refer :all]
             [bordeaux.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
